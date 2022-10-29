f62f4e91aa051a03f37ca6d4e7209a28d0c3c1f7
hanidaoud <101226230+hanidaoud@users.noreply.github.com>
2022-10-30 00:49:50 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -7,19 +7,5 @@ module.exports = {
         user     : process.env.DB_USER,
         password : process.env.DB_PASSWORD,
         database : process.env.DB_NAME
-    }/*,
-    mariadb : {
-        HOST: process.env.DB_HOST,
-        USER: process.env.DB_USER,
-        PASSWORD: process.env.DB_PASSWORD,
-        DB: process.env.DB_NAME,
-        port: process.env.DB_PORT,
-        dialect: "mysql",
-        pool: {
-            max: 5,
-            min: 0,
-            acquire: 30000,
-            idle: 10000
-        }
-    }*/
+    }
 };
